bb232112f0ddb1a31831f02e57218890b584cb14
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-05-23 18:10:50 -0300

    adiciona efeito background e centraliza nome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,19 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (<h1>JeffersonCBD</h1>)
+  return (
+    <>
+      <div className={styles.glass} />
+      <div className={styles.shapesContainer}>
+        <div className={styles.shapes}>
+          <div className={styles.circle1} />
+          <div className={styles.square} />
+          <div className={styles.circle2} />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Jefferson Carlos</h1>
+      </div>
+    </>
+  )
 }
